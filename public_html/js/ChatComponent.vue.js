83470533ec20f435f016d3e3835de6b9be7716c0
Symeon Quimby <kershoc@gmail.com>
2020-01-29 19:36:40 -0500

    remove console.log

--- public_html/js/ChatComponent.vue.js
+++ public_html/js/ChatComponent.vue.js
@@ -19,7 +19,6 @@ export default {
             let g = parseInt(hexcolor.substr(2,2),16)
             let b = parseInt(hexcolor.substr(4,2),16)
             let yiq = ((r*299)+(g*587)+(b*114))/1000
-            console.log(yiq)
             return (yiq >= 128) ? 'black' : 'white'
         }
     }
